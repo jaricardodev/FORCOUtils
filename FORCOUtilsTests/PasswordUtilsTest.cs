5b8afd9c7c4415a484cbbc11f1564aa76a07ed13
nickodev1987 <nickodev1987@gmail.com>
2015-05-16 17:49:57 -0430

    Added One-way Hashing

--- FORCOUtilsTests/PasswordUtilsTest.cs
+++ FORCOUtilsTests/PasswordUtilsTest.cs
@@ -12,9 +12,9 @@ namespace FORCOUtilsTests
         {
             const string _OriginalText = "testo de prueba";
             PasswordUtils _Utils = new PasswordUtils();
-            String _Ciphertext = _Utils.Encrypt(_OriginalText);
+            String _Ciphertext = _Utils.EncryptOneWay(_OriginalText);
            
-            Assert.AreEqual(_OriginalText, _Utils.Decrypt(_Ciphertext));
+            Assert.AreEqual(_OriginalText, _Utils.DecryptTwoWay(_Ciphertext));
         }
     }
 }
